2c4958e09df20f0bf984a494bce068722a9f4547
mateus <mateus.ramos1998@gmail.com>
2023-08-31 17:05:18 +0200

    fixed gui

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -7,15 +7,74 @@ class App:
         self.master.title("Menu")
         self.master.geometry("400x400")
         self.master.config(bg="white")  # set background color
-        self.menu = {"Cucina": {"Pizza": {"Margherita": 10, "Pepperoni": 12, "Vegetarian": 8},
-                        "Pasta": {"Spaghetti": 12, "Lasagna": 14, "Fettuccine Alfredo": 10},
-                        "Salad": {"Caesar": 8, "Greek": 10, "Caprese": 9}},
-             "Santa Maria": {"Burger": {"Cheeseburger": 9, "Bacon Burger": 11, "Veggie Burger": 8},
-                             "Fries": {"Regular": 4, "Curly": 5, "Sweet Potato": 6},
-                             "Soda": {"Coke": 2, "Sprite": 2, "Fanta": 2}},
-             "Preti": {"Steak": {"Ribeye": 20, "Filet Mignon": 25, "New York Strip": 22},
-                       "Fish": {"Salmon": 18, "Tilapia": 16, "Cod": 20},
-                       "Soup": {"Minestrone": 6, "Tomato": 5, "Chicken Noodle": 7}}}
+        self.menu = {
+
+################ Menu Cucina #############################
+            "Cucina": {
+
+
+                "Cucina": {
+                    # Pulled pork burger 9.5 vegan burger 9.5 sausage focaccia 7 cous cous 6 dolcetto 3 
+                    "Pulled pork burger": 9.5,
+                    "Vegan burger": 9.5,
+                    "Sausage focaccia": 7,
+                    "Cous cous": 6,
+                    "Dolcetto": 3
+                    }
+                },
+
+################ Menu Santa Maria #############################
+             "Santa Maria": {
+                "Burger": {
+                    "Cheeseburger": 9, 
+                    "Bacon Burger": 11, 
+                    "Veggie Burger": 8
+                    },
+                "Fries": {
+                    "Regular": 4, 
+                    "Curly": 5, 
+                    "Sweet Potato": 6
+                    }
+                },
+
+ ################ Menu preti #############################    
+             "Preti": {
+                "BIBITE": {
+                     "Chinotto Bio":3.5, 
+                     "Aranciata Bio": 3.5, 
+                     "Melagrana Bio": 3.5,
+                     "Fritz Kola": 3.5,
+                     "Fritz Kola zero": 3.5,
+                     "Acqua": 1
+                    },
+                "VINO": {
+                    "Tempesta (rosso)": 3, 
+                    "Tempesta Bottiglia" :18,
+                    "Sgass rosè": 3.5, 
+                    "Sgass rosè Bottiglia": 20,
+                    "Gran Selva": 3,
+                    "Gran Selva Bottiglia": 18,
+                    "Marescial (prosecco)": 3.5,
+                    "Marescial Bottiglia": 20
+                    },
+                "AERITIVI" : {
+                    # add spritz bianco 2 apritz aperol 3.5 spritz campari 3.5 americano 5.5
+                    "Spritz bianco" : 2,
+                    "Spritz aperol" : 3.5,
+                    "Spritz campari" : 3.5,
+                    "Americano" : 5.5
+                    },
+                "DRINKS": {
+                    # add Negroni 7 gin tonic 5.5 gin lemon 5.5 vodka tonic 5.5 vodka lemon 5.5 moscow mule 7
+                    "Negroni" : 7,
+                    "Gin tonic" : 5.5,
+                    "Gin lemon" : 5.5,
+                    "Vodka tonic" : 5.5,
+                    "Vodka lemon" : 5.5,
+                    "Moscow mule" : 7
+                    },
+                }
+            }
         self.order = []
         self.today = date.today().strftime("%Y-%m-%d")
         self.df = pd.DataFrame(columns=["Order", "Price", "Category", "Date"])
@@ -24,21 +83,26 @@ class App:
     def create_widgets(self):
         self.category_labels = []
         self.category_buttons = []
-        for i, category in enumerate(self.menu.keys()):
-            self.category_labels.append(tk.Label(self.master, text=category, fg="white"))  # set label color
-            self.category_labels[i].grid(row=i, column=0, padx=10, pady=10, sticky="w")
-            j = 1
-            for subcategory, items in self.menu[category].items():
-                self.category_labels.append(tk.Label(self.master, text=subcategory, fg="white"))  # set label color
-                self.category_labels[-1].grid(row=i, column=j, padx=10, pady=10, sticky="w")
-                j += 1
-                for k, (item, price) in enumerate(items.items()):
+        column = 0
+        for category, subcategories in self.menu.items():
+            self.category_labels.append(tk.Label(self.master, text=category, fg="white"))
+            self.category_labels[-1].grid(row=0, column=column, padx=10, pady=10, sticky="w")
+            row = 1
+            for subcategory, items in subcategories.items():
+                self.category_labels.append(tk.Label(self.master, text=subcategory, fg="white"))
+                self.category_labels[-1].grid(row=row, column=column, padx=10, pady=10, sticky="w")
+                row += 1
+                for i, (item, price) in enumerate(items.items()):
                     self.category_buttons.append(tk.Button(self.master, text=f"{item} - ${price}",
                                                             command=lambda item=item, price=price: self.add_to_order(item, price)))
-                    self.category_buttons[-1].grid(row=i, column=j+k, padx=10, pady=10)
-            i += 1
+                    self.category_buttons[-1].grid(row=row, column=column, padx=10, pady=10)
+                    if i % 3 == 2:
+                        row += 1
+                row += 1
+            column += 1
         self.print_order_button = tk.Button(self.master, text="Print Order", command=self.print_order)
-        self.print_order_button.grid(row=len(self.menu.keys())+1, column=0, padx=10, pady=10)
+        self.print_order_button.grid(row=row, column=0, columnspan=column, padx=10, pady=10)
+
     def add_to_order(self, name, price):
         self.order.append((name, price))
 
