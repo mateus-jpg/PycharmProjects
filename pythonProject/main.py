7bea8a29ba87fe9087dbd9fd89368bcd0b31c33f
mateus <mateus.ramos1998@gmail.com>
2023-08-31 14:32:15 +0200

    added subcategories

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -7,9 +7,15 @@ class App:
         self.master.title("Menu")
         self.master.geometry("400x400")
         self.master.config(bg="white")  # set background color
-        self.menu = {"Cucina": {"Pizza": 10, "Pasta": 12, "Salad": 8},
-                     "Santa Maria": {"Burger": 9, "Fries": 4, "Soda": 2},
-                     "Preti": {"Steak": 20, "Fish": 18, "Soup": 6}}
+        self.menu = {"Cucina": {"Pizza": {"Margherita": 10, "Pepperoni": 12, "Vegetarian": 8},
+                        "Pasta": {"Spaghetti": 12, "Lasagna": 14, "Fettuccine Alfredo": 10},
+                        "Salad": {"Caesar": 8, "Greek": 10, "Caprese": 9}},
+             "Santa Maria": {"Burger": {"Cheeseburger": 9, "Bacon Burger": 11, "Veggie Burger": 8},
+                             "Fries": {"Regular": 4, "Curly": 5, "Sweet Potato": 6},
+                             "Soda": {"Coke": 2, "Sprite": 2, "Fanta": 2}},
+             "Preti": {"Steak": {"Ribeye": 20, "Filet Mignon": 25, "New York Strip": 22},
+                       "Fish": {"Salmon": 18, "Tilapia": 16, "Cod": 20},
+                       "Soup": {"Minestrone": 6, "Tomato": 5, "Chicken Noodle": 7}}}
         self.order = []
         self.today = date.today().strftime("%Y-%m-%d")
         self.df = pd.DataFrame(columns=["Order", "Price", "Category", "Date"])
@@ -21,16 +27,18 @@ class App:
         for i, category in enumerate(self.menu.keys()):
             self.category_labels.append(tk.Label(self.master, text=category, fg="white"))  # set label color
             self.category_labels[i].grid(row=i, column=0, padx=10, pady=10, sticky="w")
-            self.category_buttons.append([])
-            for j, item in enumerate(self.menu[category].keys()):
-                name = item
-                price = self.menu[category][item]
-                self.category_buttons[i].append(tk.Button(self.master, text=f"{name} - ${price}",
-                                                           command=lambda name=name, price=price: self.add_to_order(name, price)))
-                self.category_buttons[i][j].grid(row=i, column=j+1, padx=10, pady=10)
+            j = 1
+            for subcategory, items in self.menu[category].items():
+                self.category_labels.append(tk.Label(self.master, text=subcategory, fg="white"))  # set label color
+                self.category_labels[-1].grid(row=i, column=j, padx=10, pady=10, sticky="w")
+                j += 1
+                for k, (item, price) in enumerate(items.items()):
+                    self.category_buttons.append(tk.Button(self.master, text=f"{item} - ${price}",
+                                                            command=lambda item=item, price=price: self.add_to_order(item, price)))
+                    self.category_buttons[-1].grid(row=i, column=j+k, padx=10, pady=10)
+            i += 1
         self.print_order_button = tk.Button(self.master, text="Print Order", command=self.print_order)
         self.print_order_button.grid(row=len(self.menu.keys())+1, column=0, padx=10, pady=10)
-
     def add_to_order(self, name, price):
         self.order.append((name, price))
 
@@ -49,7 +57,7 @@ class App:
         print(f"Data length: {len(data)}")
         df = pd.DataFrame(data=data, columns=["Order", "Price", "Category", "Date"])
         self.df = pd.concat([self.df, df], ignore_index=True)
-        self.df.to_excel(f"{self.today}_order.xlsx", index=False)
+        self.df.to_excel(f"{self.today}_order.xlsx", index=False, mode="a", header=None)
         self.order = []
         self.df = pd.DataFrame(columns=["Order", "Price", "Category", "Date"])
 
